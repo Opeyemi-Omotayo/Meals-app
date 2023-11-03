2389e29b537dce55a7c3a5cceb34ec7583158255
Opeyemi-Omotayo <opeyemi.motayo1@gmail.com>
2023-11-03 14:42:01 +0100

    meals model

---
+++ screens/Categories.js
@@ -0,0 +1,12 @@
+import { View } from "react-native"
+import { CATEGORIES } from "../data/DummyData"
+
+const Categories = () => {
+  return (
+    <View>
+        
+    </View>
+  )
+}
+
+export default Categories
