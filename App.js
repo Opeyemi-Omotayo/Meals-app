7e92fb6d3dac13b7ce24f76806d82350a0afbccb
Opeyemi-Omotayo <opeyemi.motayo1@gmail.com>
2023-11-07 18:47:30 +0100

    nested nav

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Categories from './screens/Categories';
 import { StatusBar } from 'expo-status-bar';
 import MealsOverview from './screens/MealsOverview';
 import MealDetail from './screens/MealsDetail';
+import Favorites from './screens/Favorites';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#351401' },
+        headerTintColor: 'white',
+        sceneContainerStyle: { backgroundColor: '#3f2f25' },
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={Categories}
+        options={{
+          title: 'All Categories',
+        }}
+      />
+      <Drawer.Screen name="Favorites" component={Favorites} />
+    </Drawer.Navigator>
+  );
+}
+
 
 function App() {
   return (
@@ -20,14 +45,14 @@ function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategory"
-            component={Categories}
+            name="Drawer"
+            component={DrawerNavigator}
             options={{
-              title: 'All Categories',
+              headerShown: false,
             }}
           />
         <Stack.Screen name="MealsOverview" component={MealsOverview} />
-        <Stack.Screen name="MealDetail" component={MealDetail} />
+        <Stack.Screen name="MealDetail" component={MealDetail}  options={{ title: 'About the meal'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
