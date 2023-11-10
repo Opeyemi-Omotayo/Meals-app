1fdd7803afa3875019a406d7544f21810a4fcd0f
Opeyemi-Omotayo <opeyemi.motayo1@gmail.com>
2023-11-10 14:56:16 +0100

    Favorities screen

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,7 +1,39 @@
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { useContext } from 'react';
+
+import { MEALS } from '../data/DummyData';
+import { FavoritesContext } from '../store/context/FavoritesContext';
+import MealsList from '../components/MealList/MealList';
 
 function Favorites() {
-  return <Text>The favorites screen!</Text>;
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
+  );
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
+  }
+
+  return <MealsList items={favoriteMeals} />;
 }
 
 export default Favorites;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
